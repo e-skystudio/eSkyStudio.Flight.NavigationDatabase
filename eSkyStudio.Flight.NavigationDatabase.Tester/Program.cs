4bacd468dc13f802d95e4a87f849d361bbbb0464
Romain DESSART <romaindessart@gmail.com>
2022-01-05 12:34:21 +0100

    Testing airac validity

--- eSkyStudio.Flight.NavigationDatabase.Tester/Program.cs
+++ eSkyStudio.Flight.NavigationDatabase.Tester/Program.cs
@@ -9,7 +9,15 @@ if(!File.Exists(DbPath))
     Environment.Exit(0x01);
 }
 
+
+
 NavigationDbContext database = new NavigationDbContext(DbPath);
+
+bool valid = database.Headers.First().IsAiracValid();
+string text = valid ? "Valid" : "Expired";
+Console.ForegroundColor = valid ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine($"The Current Navigation Database is {text}");
+Console.ForegroundColor = ConsoleColor.White;
 
 foreach(TerminalNdbnavaid ndb in database.TerminalNdbnavaids.Where(nav => nav.AirportIdentifier.Substring(0, 2) == "EB"))
 {
