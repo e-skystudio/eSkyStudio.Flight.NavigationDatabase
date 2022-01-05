6bf6a6edec08474281c1fa1c1b5d573951b12701
Romain DESSART <romaindessart@gmail.com>
2022-01-05 12:01:26 +0100

    herited from base class

--- eSkyStudio.Flight.NavigationDatabase/Models/TerminalWaypoint.cs
+++ eSkyStudio.Flight.NavigationDatabase/Models/TerminalWaypoint.cs
@@ -8,23 +8,9 @@ namespace eSkyStudio.Flight.NavigationDatabase.Models
 {
     [Keyless]
     [Table("tbl_terminal_waypoints")]
-    public partial class TerminalWaypoint : INavigable
+    public partial class TerminalWaypoint :EnrouteWaypoint, INavigable
     {
-        [Column("area_code", TypeName = "TEXT(3)")]
-        public string? AreaCode { get; set; }
         [Column("region_code", TypeName = "TEXT(4)")]
-        public string? RegionCode { get; set; }
-        [Column("icao_code", TypeName = "TEXT(2)")]
-        public string IcaoRegion { get; set; } = null!;
-        [Column("waypoint_identifier", TypeName = "TEXT(5)")]
-        public string Identifier { get; set; } = null!;
-        [Column("waypoint_name", TypeName = "TEXT(25)")]
-        public string? WaypointName { get; set; }
-        [Column("waypoint_type", TypeName = "TEXT(3)")]
-        public string? WaypointType { get; set; }
-        [Column("waypoint_latitude", TypeName = "DOUBLE(9)")]
-        public double? Latitude { get; set; }
-        [Column("waypoint_longitude", TypeName = "DOUBLE(10)")]
-        public double? Longitude { get; set; }
+        public string? AirportIdentifier { get; set; }
     }
 }
