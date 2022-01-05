60baa8cfcd8a310f9a846cbd0dfd57154bbf86d3
Romain DESSART <romaindessart@gmail.com>
2022-01-05 14:57:19 +0100

    Created the abstract navigable class to implement the GetBearingDistance function between INavigable

---
+++ eSkyStudio.Flight.NavigationDatabase/Models/Abstract/Navigable.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using GeographicLib;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace eSkyStudio.Flight.NavigationDatabase.Models.Abstract
+{
+    public abstract class Navigable : INavigable
+    {
+        [Column("area_code", TypeName = "TEXT(3)")]
+        public string? AreaCode { get; set; }
+        [Column("icao_code", TypeName = "TEXT(2)")]
+        public string? IcaoRegion { get; set; }
+        public abstract double Latitude { get; set;}
+        public abstract double Longitude { get; set;}
+
+        public Navigable()
+        {
+        }
+
+        public BearingDistance GetBDTo(INavigable point)
+        {
+
+            var result = Geodesic.WGS84.Inverse(Latitude, Longitude, point.Latitude, point.Longitude);
+            return new BearingDistance()
+            {
+                BearingStart = new Angle(result.Azimuth1, AngleUnit.Degree),
+                BearingEnd = new Angle(result.Azimuth2, AngleUnit.Degree),
+                Distance = new Length(result.Distance, LengthUnit.Meter),
+            };
+        }
+    }
+}
