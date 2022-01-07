7a359bc6ae30e51878d266c186377cfe36697894
Romain DESSART <romaindessart@gmail.com>
2022-01-07 12:38:30 +0100

    Reworked Terminal & Enroute Waytpoint inheritance to correct bug

--- eSkyStudio.Flight.NavigationDatabase/Models/EnrouteWaypoint.cs
+++ eSkyStudio.Flight.NavigationDatabase/Models/EnrouteWaypoint.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using eSkyStudio.Flight.NavigationDatabase.Models.Abstract;
+﻿using eSkyStudio.Flight.NavigationDatabase.Models.Abstract;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eSkyStudio.Flight.NavigationDatabase.Models
 {
     [Keyless]
     [Table("tbl_enroute_waypoints")]
-    public partial class EnrouteWaypoint : Navigable
+    public partial class EnrouteWaypoint : Waypoint
     {
-        [Column("waypoint_identifier", TypeName = "TEXT(5)")]
-        public override string Identifier { get; set; } = null!;
-        [Column("waypoint_name", TypeName = "TEXT(25)")]
-        public string? WaypointName { get; set; }
-        [Column("waypoint_type", TypeName = "TEXT(3)")]
-        public string? WaypointType { get; set; }
         [Column("waypoint_usage", TypeName = "TEXT(2)")]
         public string? WaypointUsage { get; set; }
-        [Column("waypoint_latitude", TypeName = "DOUBLE(9)")]
-        public override double Latitude { get; set; }
-        [Column("waypoint_longitude", TypeName = "DOUBLE(10)")]
-        public override double Longitude { get; set; }
     }
 }
---
+++ eSkyStudio.Flight.NavigationDatabase/Models/Waypoint.cs
@@ -0,0 +1,19 @@
+﻿using eSkyStudio.Flight.NavigationDatabase.Models.Abstract;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eSkyStudio.Flight.NavigationDatabase.Models
+{
+    public class Waypoint : Navigable
+    {
+        [Column("waypoint_identifier", TypeName = "TEXT(5)")]
+        public override string Identifier { get; set; } = null!;
+        [Column("waypoint_name", TypeName = "TEXT(25)")]
+        public string? WaypointName { get; set; }
+        [Column("waypoint_type", TypeName = "TEXT(3)")]
+        public string? WaypointType { get; set; }
+        [Column("waypoint_latitude", TypeName = "DOUBLE(9)")]
+        public override double Latitude { get; set; }
+        [Column("waypoint_longitude", TypeName = "DOUBLE(10)")]
+        public override double Longitude { get; set; }
+    }
+}
